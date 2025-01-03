940f1c9825074931ec355b29e9f4790d144dc9b5
bradyDarren <dcgbfinancial@gmail.com>
2025-01-03 12:58:52 -0600

    setup of background

---
+++ main.py
@@ -0,0 +1,17 @@
+from turtle import Screen
+
+# background setup 
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=1250, height=1250)
+screen.title("PONG GAME")
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
