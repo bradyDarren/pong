91d31489e2c6bcda5ffbc21b426fba8dbd58e120
bradyDarren <dcgbfinancial@gmail.com>
2025-01-04 16:38:54 -0600

    addition of code to test other classes

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from turtle import Screen
+from scoreboard import Scoreboard
 
 # background setup 
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(width=1250, height=1250)
+screen.setup(width=1250, height=1000)
 screen.title("PONG GAME")
 
-
-
-
+scoreboard = Scoreboard()
 
 
 
