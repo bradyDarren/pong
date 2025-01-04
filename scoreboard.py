f7e91daa10d2b16e0dad08636e0d157792be0b4a
Darren Brady <dcgbfinancial@gmail.com>
2025-01-04 12:15:57 +0000

    addition of the update_scoreboard method and create net method

--- scoreboard.py
+++ scoreboard.py
@@ -1,3 +1,36 @@
 from turtle import Turtle
 
-class Scoreboard: 
+x_cor = 0
+y_cor = 615
+color = "white"
+
+# constructor - creates scoreboard
+class Scoreboard(Turtle): 
+    def __init__(self):
+        super.__init__(shape = "square")
+        self.p1_score = 0 
+        self.p2_score = 0
+        self.color(color, color)
+        self.goto(x= x_cor,y= y_cor)
+        self.net = []
+        self.create_net()
+        self.update_scorboard()
+
+# creates scoreboard - displays p1 and p2 current score.
+    def update_scorboard(self):
+        self.clear()
+        self.write()
+        
+
+
+# creates the net in the middle of the screen.
+    def create_net(self):
+        while self.net[-1::].ycor() > -y_cor:
+            net_section = Turtle()
+            net_section.color(color,color)
+            net_section.goto(x=x_cor,y=y_cor)
+
+
+
+
+
