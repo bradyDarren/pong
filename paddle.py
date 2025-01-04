14088b2d0550356ef27c28003e23dcdabeaf2eb8
Darren Brady <dcgbfinancial@gmail.com>
2025-01-04 14:41:12 +0000

    refactoring of Paddle class

--- paddle.py
+++ paddle.py
@@ -2,7 +2,7 @@ from turtle import Turtle
 
 # set inital coordinates of paddle
 class Paddle: 
-    def __init__(self, paddle_shape = "square", xcord, ycord, color = "white"):
+    def __init__(self, xcord, ycord, color = "white", paddle_shape = "square"):
         self.xcord = xcord 
         self.ycord = ycord
         self.paddle_shape = paddle_shape
@@ -13,5 +13,8 @@ class Paddle:
         for i in range(4):
             paddle_segment = Turtle(shape=self.paddle_shape)
             paddle_segment.penup()
-            
+            paddle_segment.color(self.color, self.color)
+            paddle_segment.goto(x = self.xcord, y = self.ycord + (20 * i))
+            self.segments.append(paddle_segment)
+
 
