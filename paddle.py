7b19110c08f4d6a0e19178b3592ac47b517b3e72
bradyDarren <dcgbfinancial@gmail.com>
2025-01-04 16:39:14 -0600

    additional commit

--- paddle.py
+++ paddle.py
@@ -1,29 +1,27 @@
 from turtle import Turtle
 
 # set inital coordinates of paddle
-class Paddle: 
-    def __init__(self, xcord, ycord, color = "white", paddle_shape = "square"):
+class Paddles: 
+    def __init__(self, xcord, ycord, color = "white", paddle_shape = "square", heading = 90):
         self.xcord = xcord 
         self.ycord = ycord
         self.paddle_shape = paddle_shape
         self.color = color
-        self.segments = []
+        self.heading = heading
+        self.two_paddles = []
         self.create_paddle()
+
     
     def create_paddle(self):
-        for i in range(4):
-            paddle_segment = Turtle(shape=self.paddle_shape)
-            paddle_segment.penup()
-            paddle_segment.setheading(270)
-            paddle_segment.color(self.color, self.color)
-            paddle_segment.goto(x = self.xcord, y = self.ycord + (20 * i))
-            self.segments.append(paddle_segment)
+        paddle = Turtle(self.paddle_shape)
+        paddle.setheading(self.heading)
+        paddle.goto(x = self.xcord, y = self.ycord)
+        paddle.penup()
+        paddle.color(self.color, self.color)
+        paddle.shapesize(stretch_len = .5, stretch_wid = 4)
     
-    def move(self):
-        for section in range(len(self.segments) - 1, 0, -1):
-            new_x = self.segments(section - 1).xcor()
-            new_y = self.segments(section - 1).ycor()
-            self.segments[section].goto(x=new_x, y= new_y)
-
-    def up(self):
-        self.segments[0].setheading(270)
+    def move_up(self, player):
+        self.two_paddles[player - 1].forward(10)
+    
+    def move_down(self, player):
+        self.two_paddles[player - 1].backward(10)
