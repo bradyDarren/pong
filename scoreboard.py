b72a83cac08d9af0c78bb2298814f62d9085fff2
bradyDarren <dcgbfinancial@gmail.com>
2025-01-04 16:40:41 -0600

    adjustments for functional code

--- scoreboard.py
+++ scoreboard.py
@@ -5,8 +5,10 @@ from turtle import Turtle
 
 # constructor - creates scoreboard
 class Scoreboard(Turtle): 
-    def __init__(self, x_cor = 0, y_cor = 615, color = "white"):
-        super.__init__()
+    def __init__(self, x_cor = 0, y_cor = 450, color = "white"):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
         self.p1_score = 0 
         self.p2_score = 0
         self.color(color, color)
@@ -18,18 +20,26 @@ class Scoreboard(Turtle):
 # creates scoreboard - displays p1 and p2 current score.
     def update_scorboard(self):
         self.clear()
-        self.write(f"{self.p1_score}          {self.p2_score}")
+        self.write(f"{self.p1_score}          {self.p2_score}", move = False, align = "center", font = ("Times New Roman", 40, "normal"))
 
-# takes winning player for that set and increases that player score.  
+# # takes winning player for that set and increases that player score.  
     def increase_score(self, player):
         if player == 1:
             self.p1_score += 1
         elif player == 2:
-            self.p2_score += 1
+            self.p2_score += 2
         self.update_scorboard()
     
-# creates the net in the middle of the screen.
- 
+# # creates the net in the middle of the screen.
+    def create_net(self):
+        if self.net[-1::].ycor() > -y_cor:
+            for _ in range(3):
+                net_section = Turtle()
+                net_section.color(self.color,self.color)
+                net_section.goto(x=self.x_cor,y=self.y_cor)
+                self.net.append(net_section)
+                y_cor -= 20
+            y_cor -= 60
 
 
 
