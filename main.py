a615d3ccbfb3a5f7d24c2d36e3a0aaef80b09365
bradyDarren <dcgbfinancial@gmail.com>
2025-01-06 17:43:43 -0600

    working on paddle movement and appearance

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from turtle import Screen
 from scoreboard import Scoreboard
+from paddle import Paddles
 
 # background setup 
 window = Screen()
 window.bgcolor("black")
+
+#set window size & title
 window.setup(width=1250, height=1000)
 window_height = window.window_height()
 window_width = window.window_width()
 window.title("PONG GAME")
 
+window.listen()
 
-scoreboard = Scoreboard()
-scoreboard.create_net(window_height)
+# scoreboard = Scoreboard()
+# scoreboard.create_net(window_height)
 
+paddle_one = Paddles(xcord = -600, ycord= 0)
 
 
 
