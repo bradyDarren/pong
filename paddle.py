2ab50209b1a3d27c40bfa49625d02e9945d08a7c
bradyDarren <dcgbfinancial@gmail.com>
2025-01-08 17:14:54 -0600

    addition of the a for loop within the create_paddles function allows us to be able to create two seperate paddles at different ends of the screen

--- paddle.py
+++ paddle.py
@@ -12,13 +12,17 @@ class Paddles:
         self.create_paddle()
 
     def create_paddle(self):
-        paddle = Turtle(self.paddle_shape)
-        paddle.penup()
-        paddle.setheading(self.heading)
-        paddle.color(self.color, self.color)
-        paddle.shapesize(stretch_len = 3, stretch_wid = .5)
-        paddle.goto(x = self.xcord, y = self.ycord)
-        self.two_paddles.append(paddle)
+        for paddle in range (2):
+            paddle = Turtle(self.paddle_shape)
+            paddle.penup()
+            paddle.setheading(self.heading)
+            paddle.color(self.color, self.color)
+            paddle.shapesize(stretch_len = 3, stretch_wid = .5)
+            if len(self.two_paddles) != 1:
+                paddle.goto(x = -self.xcord, y = self.ycord)
+            else:
+                paddle.goto(x = self.xcord, y = self.ycord)
+            self.two_paddles.append(paddle)
 
 # figure out how to take an input of the player playing and respond using the keystroke.
     def move_up(self):
