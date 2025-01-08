417ce9d912669d930d0ed1979408f2db6a70984d
bradyDarren <dcgbfinancial@gmail.com>
2025-01-07 18:41:47 -0600

    additional commit

--- paddle.py
+++ paddle.py
@@ -11,15 +11,15 @@ class Paddles:
         self.two_paddles = []
         self.create_paddle()
 
-    
     def create_paddle(self):
         paddle = Turtle(self.paddle_shape)
-        paddle.setheading(self.heading)
-        paddle.goto(x = self.xcord, y = self.ycord)
         paddle.penup()
+        paddle.setheading(self.heading)
         paddle.color(self.color, self.color)
-        paddle.shapesize(stretch_len = .5, stretch_wid = 4)
-    
+        paddle.shapesize(stretch_len = 3, stretch_wid = .5)
+        paddle.goto(x = self.xcord, y = self.ycord)
+        self.two_paddles.append(paddle)
+       
     def move_up(self, player):
         self.two_paddles[player - 1].forward(10)
     
