cc8100db8a89e74d3c2966ea19cffc337fcdceb3
bradyDarren <dcgbfinancial@gmail.com>
2025-01-08 17:22:55 -0600

    addition of comments

--- paddle.py
+++ paddle.py
@@ -11,6 +11,7 @@ class Paddles:
         self.two_paddles = []
         self.create_paddles()
 
+# creation of two padddles for player #1 and player #2
     def create_paddles(self):
         for paddle in range (2):
             paddle = Turtle(self.paddle_shape)
@@ -24,13 +25,14 @@ class Paddles:
                 paddle.goto(x = self.xcord, y = self.ycord)
             self.two_paddles.append(paddle)
 
-# figure out how to take an input of the player playing and respond using the keystroke.
+# movement functions for the first paddle.
     def move_up_1(self):
         self.two_paddles[0].forward(20)
     
     def move_down_1(self):
         self.two_paddles[0].backward(20)
-    
+
+# movement functions for the second paddle.
     def move_up_2(self):
         self.two_paddles[1].forward(20)
     
