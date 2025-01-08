7aed8242cecc5f0049e4c5953e9c42302e77640d
bradyDarren <dcgbfinancial@gmail.com>
2025-01-07 18:52:31 -0600

    addition of the onkey methods allowing us to be able to move our paddle at the press of a button

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ window.listen()
 
 paddle_one = Paddles(xcord = -600, ycord= 0)
 
+window.onkey(key="Up",fun=paddle_one.move_up)
+window.onkey(key="Down",fun=paddle_one.move_down)
 
 
 window.exitonclick()
