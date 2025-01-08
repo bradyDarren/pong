aadc9b56d1e9c8f3cc42b57025b582e2e28867cd
bradyDarren <dcgbfinancial@gmail.com>
2025-01-08 17:20:50 -0600

    addition of the movement functions for paddle #2

--- paddle.py
+++ paddle.py
@@ -9,15 +9,15 @@ class Paddles:
         self.color = color
         self.heading = heading
         self.two_paddles = []
-        self.create_paddle()
+        self.create_paddles()
 
-    def create_paddle(self):
+    def create_paddles(self):
         for paddle in range (2):
             paddle = Turtle(self.paddle_shape)
             paddle.penup()
             paddle.setheading(self.heading)
             paddle.color(self.color, self.color)
-            paddle.shapesize(stretch_len = 3, stretch_wid = .5)
+            paddle.shapesize(stretch_len = 4, stretch_wid = .5)
             if len(self.two_paddles) != 1:
                 paddle.goto(x = -self.xcord, y = self.ycord)
             else:
@@ -25,8 +25,14 @@ class Paddles:
             self.two_paddles.append(paddle)
 
 # figure out how to take an input of the player playing and respond using the keystroke.
-    def move_up(self):
-        self.two_paddles[0].forward(10)
+    def move_up_1(self):
+        self.two_paddles[0].forward(20)
     
-    def move_down(self):
-        self.two_paddles[0].backward(10)
+    def move_down_1(self):
+        self.two_paddles[0].backward(20)
+    
+    def move_up_2(self):
+        self.two_paddles[1].forward(20)
+    
+    def move_down_2(self):
+        self.two_paddles[1].backward(20)
