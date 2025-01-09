76c528f166408af7a2dadef62559fce4eb977a32
bradyDarren <dcgbfinancial@gmail.com>
2025-01-08 18:05:25 -0600

    addition of the while loop

--- main.py
+++ main.py
@@ -7,17 +7,20 @@ window = Screen()
 window.bgcolor("black")
 
 #set window size & title
-window.setup(width=1250, height=1000)
+window.setup(width=800, height=600)
 window_height = window.window_height()
 window_width = window.window_width()
 window.title("PONG GAME")
+
+# removes the automatic animation from our window.
+window.tracer(0)
 
 window.listen()
 
 # scoreboard = Scoreboard()
 # scoreboard.create_net(window_height)
 
-paddles = Paddles(xcord = -600, ycord= 0)
+paddles = Paddles(xcord = -350, ycord= 0)
 
 # key functions that enable paddle #1 to move up and down. 
 window.onkey(key="Up",fun=paddles.move_up_1)
@@ -27,5 +30,9 @@ window.onkey(key="Down",fun=paddles.move_down_1)
 window.onkey(key="a",fun=paddles.move_up_2)
 window.onkey(key="z",fun=paddles.move_down_2)
 
+game_on = True
+
+while game_on: 
+    window.update()
 
 window.exitonclick()
