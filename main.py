64bfb04baaaf35bf968e7c0b961faf3f1e095d7f
bradyDarren <dcgbfinancial@gmail.com>
2025-01-10 06:57:26 -0600

    must remember to not call th function using the on key method

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ window.bgcolor("black")
 
 #set window size & title
 window.setup(width=800, height=600)
-window_height = window.window_height()
-window_width = window.window_width()
 window.title("PONG GAME")
 
 # removes the automatic animation from our window.
@@ -20,15 +18,14 @@ window.listen()
 # scoreboard = Scoreboard()
 # scoreboard.create_net(window_height)
 
-paddles = Paddles(xcord = -350, ycord= 0)
+paddle_1 = Paddles((-370,0))
+paddle_2 = Paddles((370,0))
 
-# key functions that enable paddle #1 to move up and down. 
-window.onkey(key="Up",fun=paddles.move_up_1)
-window.onkey(key="Down",fun=paddles.move_down_1)
+window.onkey(key = "Up", fun = paddle_1.move_up)
+window.onkey(key = "Down", fun = paddle_1.move_down)
 
-# key functions that enable paddle #2 to move up and down. 
-window.onkey(key="a",fun=paddles.move_up_2)
-window.onkey(key="z",fun=paddles.move_down_2)
+window.onkey(key = "a", fun = paddle_2.move_up)
+window.onkey(key = "z", fun = paddle_2.move_down)
 
 game_on = True
 
