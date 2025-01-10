b43074fece9c8668aebc1083aac1c64896b4c40a
bradyDarren <dcgbfinancial@gmail.com>
2025-01-10 06:42:32 -0600

    removed the create_paddle function and inherited the Turtle class

--- paddle.py
+++ paddle.py
@@ -2,40 +2,25 @@ from turtle import Turtle
 
 # set inital coordinates of paddle
 # adjust this to inherit the Turtle Class and remove the create_paddles method.
-class Paddles(): 
-    def __init__(self, xcord, ycord, color = "white", paddle_shape = "square", heading = 90):
-        self.xcord = xcord 
-        self.ycord = ycord
-        self.paddle_shape = paddle_shape
-        self.color = color
-        self.heading = heading
-        self.two_paddles = []
-        self.create_paddles()
-
-# creation of two padddles for player #1 and player #2
-    def create_paddles(self):
-        for paddle in range (2):
-            paddle = Turtle(self.paddle_shape)
-            paddle.penup()
-            paddle.setheading(self.heading)
-            paddle.color(self.color, self.color)
-            paddle.shapesize(stretch_len = 5, stretch_wid = 1)
-            if len(self.two_paddles) != 1:
-                paddle.goto(x = -self.xcord, y = self.ycord)
-            else:
-                paddle.goto(x = self.xcord, y = self.ycord)
-            self.two_paddles.append(paddle)
+class Paddles(Turtle):
+    def __init__(self, coordinates):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_len = .5, stretch_wid = 5)
+        self.penup()
+        self.goto(coordinates)
 
 # movement functions for the first paddle.
     def move_up_1(self):
-        self.two_paddles[0].forward(20)
+        self.goto(self.xcor(), (self.ycor() + 20))
     
-    def move_down_1(self):
-        self.two_paddles[0].backward(20)
+#     def move_down_1(self):
+#         self.two_paddles[0].backward(20)
 
-# movement functions for the second paddle.
-    def move_up_2(self):
-        self.two_paddles[1].forward(20)
+# # movement functions for the second paddle.
+#     def move_up_2(self):
+#         self.two_paddles[1].forward(20)
     
-    def move_down_2(self):
-        self.two_paddles[1].backward(20)
+#     def move_down_2(self):
+#         self.two_paddles[1].backward(20)
